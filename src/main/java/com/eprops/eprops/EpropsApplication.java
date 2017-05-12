097676822dfa59d817f67cf1a6a82812c36c444a
ciprian-constantin.hurmuzache <ciprian-constantin.hurmuzache@sky.uk>
2017-05-12 13:59:48 +0100

    Added initial files

---
+++ src/main/java/com/eprops/eprops/EpropsApplication.java
@@ -0,0 +1,12 @@
+package com.eprops.eprops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpropsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eprops/eprops/EpropsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eprops.eprops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EpropsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
